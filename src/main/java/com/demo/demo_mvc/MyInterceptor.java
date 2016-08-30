32024bbd60afd49a0c58eab617d7694090c827c5
paletter <234866877@qq.com>
2016-08-30 14:45:36 +0800

    INIT

---
+++ src/main/java/com/demo/demo_mvc/MyInterceptor.java
@@ -0,0 +1,37 @@
+package com.demo.demo_mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class MyInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("1.preHandle");
+		System.out.println(handler);
+		return super.preHandle(request, response, handler);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		System.out.println("2.postHandle");
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+		System.out.println("3.afterCompletion");
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+	@Override
+	public void afterConcurrentHandlingStarted(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("4.afterConcurrentHandlingStarted");
+		super.afterConcurrentHandlingStarted(request, response, handler);
+	}
+
+	
+}
---
+++ src/main/java/com/demo/demo_mvc/StartTestController.java
@@ -0,0 +1,37 @@
+package com.demo.demo_mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class StartTestController {
+
+	@RequestMapping(value="/StartTestController/startTest.do", method = RequestMethod.GET)
+	public String startTest(HttpServletRequest request) {
+		System.out.println("Start test!");
+		return "/login"; 
+	}
+	
+	@RequestMapping(value="/welcome.do", method = RequestMethod.GET)
+	public String welcome() {
+		return "/welcome"; 
+	}
+
+	@RequestMapping(value="/login.do", method = RequestMethod.GET)
+	public String login(HttpServletRequest request) {
+		String u = request.getParameter("u");
+		String p = request.getParameter("p");
+		
+		if(u.equals("1") && p.equals("2")) return "/success";
+		else return "/error"; 
+	}
+	
+	@RequestMapping(value="/anyone.do", method = RequestMethod.GET)
+	public String anyone() {
+		System.out.println("Anyone");
+		return "/anyone"; 
+	}
+}
